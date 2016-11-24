168a8234298f60d2617def250f164db8958fbe15
Giorgos Demosthenous <georgedemo2000@gmail.com>
2016-11-24 11:58:49 +0200

    Created addAppointment activity and view

--- Vivere/app/src/main/java/com/vivere/app/vivere/Fragments/AppointmentsFragment.java
+++ Vivere/app/src/main/java/com/vivere/app/vivere/Fragments/AppointmentsFragment.java
@@ -1,7 +1,9 @@
 package com.vivere.app.vivere.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +12,9 @@ import android.widget.ListView;
 
 import com.vivere.app.vivere.R;
 import com.vivere.app.vivere.adapters.AppointmentsAdapter;
+import com.vivere.app.vivere.addAppointment;
 import com.vivere.app.vivere.models.Appointment;
 
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -26,6 +28,7 @@ public class AppointmentsFragment extends Fragment {
     private ListView appList;
     private ArrayList<Appointment> appointments= new ArrayList<>();
     private AppointmentsAdapter appAdapter;
+    private FloatingActionButton appActionBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -40,6 +43,7 @@ public class AppointmentsFragment extends Fragment {
             parent.removeView(view);
         }
 
+        appActionBtn = (FloatingActionButton) view.findViewById(R.id.appointmentsActionBtn);
         appAdapter = new AppointmentsAdapter(getActivity(),R.layout.appointments_item);
         appList = (ListView) view.findViewById(R.id.appointmentsListView);
         appList.setAdapter(appAdapter);
@@ -52,6 +56,14 @@ public class AppointmentsFragment extends Fragment {
         Appointment app2 = new Appointment("Dr Who","Kakos",d,"Hurts");
         appointments.add(app2);
         setListData(app2);
+
+        appActionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), addAppointment.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
 
---
+++ Vivere/app/src/main/java/com/vivere/app/vivere/addAppointment.java
@@ -0,0 +1,15 @@
+package com.vivere.app.vivere;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by User on 24/11/2016.
+ */
+
+public class addAppointment extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_appointment);
+    }
+}
