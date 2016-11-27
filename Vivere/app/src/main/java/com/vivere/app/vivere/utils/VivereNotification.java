bc0320ef96bdf9c3f13024d26c56b5ad4d1393d8
Kyriakos Kyriakou <kyriakosb4a@yahoo.gr>
2016-11-27 15:40:33 +0200

    Changes for package utils.

---
+++ Vivere/app/src/main/java/com/vivere/app/vivere/utils/VivereNotification.java
@@ -0,0 +1,21 @@
+package com.vivere.app.vivere.utils;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
+
+import com.vivere.app.vivere.MainActivity;
+import com.vivere.app.vivere.R;
+
+/**
+ * Created by kyria_000 on 27/11/2016.
+ */
+
+public class VivereNotification extends AppCompatActivity {
+
+
+}
