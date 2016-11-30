ddf80c83607cc7326c511d407a195b72fc019235
Kyriakos Kyriakou <kyriakosb4a@hotmail.com>
2016-12-01 00:13:47 +0200

    Exam Changes for advice

--- RESTful Services/insertExam.php
+++ RESTful Services/insertExam.php
@@ -10,9 +10,10 @@ $muser_name= $_POST["msuser_name"];
 $id =$_POST["id"];
 $type =$_POST["type"];
 $date = $_POST["date"];
+$advice = $_POST["advice"]
 $res = "TBA";
 
-$sql= "INSERT INTO Exam VALUES('$puser_name','$msuser_name','$id','$type','$res','$date');";
+$sql= "INSERT INTO Exam VALUES('$puser_name','$msuser_name','$id','$type','$res','$date', '$advice');";
 
 
 if(mysqli_query($conn,$sql)){
