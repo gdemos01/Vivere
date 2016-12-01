377b23d5eabbfdd1cc61a1e11b15dd1890cc4dc7
Kyriakos Kyriakou <kyriakosb4a@yahoo.gr>
2016-12-01 02:43:52 +0200

    VivereNotificationService implemented with problems in handler for the runable object.

---
+++ Vivere/app/src/main/java/com/vivere/app/vivere/VivereNotificationService.java
@@ -0,0 +1,85 @@
+package com.vivere.app.vivere;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions and extra parameters.
+ */
+public class VivereNotificationService extends IntentService {
+    private static final String TAG = VivereNotificationService.class.getSimpleName();
+    public static final int NOTIFICATION_ID = 123;
+
+    private Handler mHandler;
+
+    public VivereNotificationService() {
+        super("NotificationService");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        mHandler = new Handler();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.v(TAG, "In onHandleIntent. Message will be printed after 10sec");
+        if (intent != null) {
+
+            synchronized (this){
+                try {
+                    wait(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.v(TAG, "Service started");
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Service Completed" , Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+        }
+    }
+
+    public void createNotification(){
+
+        Intent intent = new Intent(this, MainActivity.class);
+
+        TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(this);
+        taskStackBuilder.addParentStack(MainActivity.class);
+        taskStackBuilder.addNextIntent(intent);
+
+        PendingIntent pendingIntent = taskStackBuilder.
+                getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText("Sample Notification")
+                .setAutoCancel(true)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setContentIntent(pendingIntent)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, notification);
+
+    }
+}
