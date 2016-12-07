db69d30c710c3f12b38ebfcb1af09dd4741eacde
Giorgos Demosthenous <georgedemo2000@gmail.com>
2016-12-07 17:35:55 +0200

    Added notification service for Appointments

--- Vivere/app/src/main/java/com/vivere/app/vivere/addAppointmentTime.java
+++ Vivere/app/src/main/java/com/vivere/app/vivere/addAppointmentTime.java
@@ -1,8 +1,13 @@
 package com.vivere.app.vivere;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ListView;
@@ -11,6 +16,7 @@ import android.widget.TextView;
 import com.vivere.app.vivere.adapters.AvailableHoursAdapter;
 import com.vivere.app.vivere.db.DatabaseHelper;
 import com.vivere.app.vivere.models.Appointment;
+import com.vivere.app.vivere.notification.NotificationPublisher;
 import com.vivere.app.vivere.services.GetCurrentDateAppointments;
 import com.vivere.app.vivere.services.InsertAppointment;
 
@@ -109,6 +115,23 @@ public class addAppointmentTime extends AppCompatActivity {
                 InsertAppointment insertAppointment = new InsertAppointment();
                 insertAppointment.execute("john",ms_selected,t.toString(),description);
                 db.addAppointment(app);
+
+                /**
+                 * Scheduling notifications for 1 day before and 1 hour before
+                 */
+                long oneDayBefore = app.getDate().getTime() - (long)(24*60*60*1000);
+                long oneHourBefore = app.getDate().getTime() -(long)(60*60*1000);
+                int randomid = (int) (Math.random() * 2000000000);
+                int randomid2 = (int) (Math.random() * 2000000000);
+                int randomid3 = (int) (Math.random() * 2000000000);
+
+                scheduleNotification(getNotification("Appointment Reminder",app.getDescription(),
+                        app.getDate().toString()),5000,15,false,800+randomid);
+                scheduleNotification(getNotification("Appointment Reminder",app.getDescription(),
+                        app.getDate().toString()),oneDayBefore,15,false,800+randomid2);
+                scheduleNotification(getNotification("Appointment Reminder",app.getDescription(),
+                        app.getDate().toString()),oneHourBefore,15,false,800+randomid3);
+
                 Intent intent = new Intent(addAppointmentTime.this,MainActivity.class);
                 startActivity(intent);
             }
@@ -129,6 +152,63 @@ public class addAppointmentTime extends AppCompatActivity {
         avAdapter.setSelectedItem(pos);
         avAdapter.notifyDataSetChanged();
         selectedTime = avAdapter.getSelectedItem();
+    }
+
+    public void scheduleNotification(Notification notification, long delay,
+                                     int timeInterval,boolean isScheduled,int notificationID) {
+
+        Intent notificationIntent = new Intent(this, NotificationPublisher.class);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, notificationID);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, notificationID, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        long futureInMillis = SystemClock.elapsedRealtime() + delay;
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        if (!isScheduled) {
+            alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
+        } else {
+            switch (timeInterval) {
+                case 15: {
+                    //15 min
+                    alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                            SystemClock.elapsedRealtime() + AlarmManager.INTERVAL_FIFTEEN_MINUTES,
+                            AlarmManager.INTERVAL_FIFTEEN_MINUTES, pendingIntent);
+                    break;
+                }
+                case 30: {
+                    //30 min
+                    alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                            SystemClock.elapsedRealtime() + AlarmManager.INTERVAL_HALF_HOUR,
+                            AlarmManager.INTERVAL_HALF_HOUR, pendingIntent);
+                    break;
+                }
+                case 60: {
+                    //60 min
+                    alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                            SystemClock.elapsedRealtime() + AlarmManager.INTERVAL_HOUR,
+                            AlarmManager.INTERVAL_HOUR, pendingIntent);
+                    break;
+                }
+                default: {//for a whole day
+                    alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                            SystemClock.elapsedRealtime() + AlarmManager.INTERVAL_DAY,
+                            AlarmManager.INTERVAL_DAY, pendingIntent);
+                    break;
+                }
+            }
+
+
+        }
+    }
+
+    public Notification getNotification(String title, String content, String sub) {
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle(title);
+        builder.setContentText(content);
+        builder.setContentText(content);
+        builder.setSubText(sub);
+        builder.setSmallIcon(R.drawable.vivere_logo_bw2);
+        return builder.build();
     }
 
 }
